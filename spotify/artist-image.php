43d3bf84ea3909d1d04e3fe412c61abce93f3182
Marcelo Alves <marcelonralves@gmail.com>
2025-04-18 02:01:20 -0300

    feat: header para evitar problema com CORS e tamanho mínimo de imagem

--- spotify/artist-image.php
+++ spotify/artist-image.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 require_once __DIR__ . '/../spotify/spotify_token.php';
 
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
 
 if (!isset($_GET['q']) || trim($_GET['q']) === '') {
     http_response_code(400);
@@ -40,6 +41,16 @@ if (empty($data['artists']['items'])) {
 
 $artist = $data['artists']['items'][0];
 $image = $artist['images'][0]['url'] ?? null;
+
+foreach($data['artists']['items'][0]['images'] as $image) {
+    if (
+        $image['height'] > 300 &&
+        $image['width'] > 300
+    ) {
+        $image = $image['url'];
+        break;
+    }
+}
 
 echo json_encode([
     'nome' => $artist['name'],
